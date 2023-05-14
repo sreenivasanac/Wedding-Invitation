4ae58a3932ccd186ab601b5444817ed76d9b48dc
Sreenivasan AC <sreenivasan.nitt@gmail.com>
2023-05-15 01:58:48 +0530

    few more changes to website, including time difference for Kalyaanam

--- js/script.js
+++ js/script.js
@@ -12,7 +12,7 @@ $(document).on('click', function(){
 });
 
 // Set the date we're counting down to
-var countDownDate = new Date("May 21, 2023 00:00:00").getTime();
+var countDownDate = new Date("May 21, 2023 08:30:00").getTime();
 
 // Update the count down every 1 second
 var x = setInterval(function() {
